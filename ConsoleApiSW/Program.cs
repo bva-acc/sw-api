9d32d6893f4bc1351342737e6d0a7ede11920f80
Vadim Bulat <justliveinlife@gmail.com>
2020-11-11 14:44:28 +0300

    Add console app

---
+++ ConsoleApiSW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsoleApiSW
+{
+   
+    class Program
+    {
+
+        static HttpClient client = new HttpClient();
+
+        static void ShowProduct(string content)
+        {
+            Console.WriteLine(content);
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://swapi.dev/api/planets/4/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+               var content = await client.GetStringAsync("https://swapi.dev/api/planets/4/");
+               ShowProduct(content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
